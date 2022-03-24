d1e005a700f6e6f2d26f99fb5242bb687bb2a0bd
Javier Carmona G <javier.carmona@wizeline.com>
2022-03-24 12:41:28 -0600

    ADD: Added Section 13: House Marketplace Project Start

---
+++ src/firebase.config.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firesotre';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDtrOt9-ohDW_7g4ol35N_1bnIpemWfQlU',
+
+  authDomain: 'house-marketplace-app-8c39d.firebaseapp.com',
+
+  projectId: 'house-marketplace-app-8c39d',
+
+  storageBucket: 'house-marketplace-app-8c39d.appspot.com',
+
+  messagingSenderId: '532560228656',
+
+  appId: '1:532560228656:web:d5d4dd159316379baed8f5',
+};
+
+export const db = getFirestore();
